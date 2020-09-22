2527362245383a2abc2cf570747fb3795664d77e
Lovász Ákos <akos4x44@gmail.com>
2020-09-22 14:35:36 +0200

    renamed first project and added an if statement

--- CppBalla/HelloWorld.cpp
+++ CppBalla/HelloWorld.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
-using namespace std;
+using namespace std; //lehet using nélkül is pl std::cout
 
+//alapból nincs üres string paraméter mint pl c#-ban
 int main() {
     cout << "Hello World!" << endl;
+    //endl >> "hello xd" >> cout; MÉNEM LEHET VISSZAFÉ
+    if (1 != 2)
+    {
+        cout << "1 != 2 lol\n";
+    }
     return 0;
 }
