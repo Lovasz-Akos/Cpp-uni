05cce4243a0a6666a9b2c1088d9acab692b37ed7
Lovász Ákos <akos4x44@gmail.com>
2020-09-29 14:07:51 +0200

    added check for getNumber result

--- Multiplication Tabée/Multiplication Tabée.cpp
+++ Multiplication Tabée/Multiplication Tabée.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
+int GetNumber();
 
 using namespace std;
 
 int main()
 {
     int tableSize = GetNumber();
+
+    /*int whole;
+
+    cout << "Please enter a whole number: ";
+
+    if (cin >> whole)
+        cout << "good\n";
+    else {
+        cout << "bad\n";
+    }
+
+    double fraction;
+
+    cout << "Please enter a fraction: ";
+    cin >> fraction;
+
+    cout << whole << " " << fraction << endl;
+    */
 }
 
 int GetNumber() {
     int number;
-    
-    cout << "Please enter a number: ";
-    cin >> number;
+    do {
+        cout << "Please enter a number: ";
+        cin >> number;
+    } while (!(cin >> number));
 
     return number;
 }
